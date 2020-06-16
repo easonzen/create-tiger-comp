f03b13650ebe35e8fc04ec9b62dda49587139363
easonzen <scnznl@gmail.com>
2020-06-16 19:42:33 +0800

    删除多余配置

--- src/packageTemp.js
+++ src/packageTemp.js
@@ -3,9 +3,6 @@ module.exports = {
     module: 'dist/index.esm.js',
     types: 'dist/index.d.ts',
     files: ['LICENSE', 'dist', 'src'],
-    scripts: {
-        build: 'father-build'
-    },
     peerDependencies: {
         '@babel/runtime': '^7.0.0',
         react: '^16.13.1',
